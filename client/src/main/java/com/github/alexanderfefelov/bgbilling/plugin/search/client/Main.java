011640a425464cb1e4fc21387bfbb7038341bddf
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-01 04:00:22 +0300

    Implement `client` module

---
+++ client/src/main/java/com/github/alexanderfefelov/bgbilling/plugin/search/client/Main.java
@@ -0,0 +1,16 @@
+package com.github.alexanderfefelov.bgbilling.plugin.search.client;
+
+import ru.bitel.bgbilling.client.common.BGUTabPanel;
+
+public class Main extends BGUTabPanel {
+
+    public Main() throws Exception {
+        super();
+        tabTitle = "Поиск";
+    }
+
+    @Override
+    protected void jbInit() throws Exception {
+    }
+
+}
