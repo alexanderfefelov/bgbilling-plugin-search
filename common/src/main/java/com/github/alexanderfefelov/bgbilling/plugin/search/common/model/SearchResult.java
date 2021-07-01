d4a41e1451fc2c78c008d83de4c78677c0c56014
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-01 03:46:57 +0300

    Implement `common` module

---
+++ common/src/main/java/com/github/alexanderfefelov/bgbilling/plugin/search/common/model/SearchResult.java
@@ -0,0 +1,15 @@
+package com.github.alexanderfefelov.bgbilling.plugin.search.common.model;
+
+public class SearchResult {
+
+    public Long getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(Long contractId) {
+        this.contractId = contractId;
+    }
+
+    private Long contractId;
+
+}
