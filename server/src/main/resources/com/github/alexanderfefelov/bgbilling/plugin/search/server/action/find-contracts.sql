8e26a93d803b61ba097c24ef3f498a67a76892e7
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-03 08:30:22 +0300

    Refactoring

--- server/src/main/resources/com/github/alexanderfefelov/bgbilling/plugin/search/server/action/find-contracts.sql
+++ server/src/main/resources/com/github/alexanderfefelov/bgbilling/plugin/search/server/action/find-contracts.sql
@@ -1,94 +1,69 @@
 select
   group_concat(x.source separator '\n') as 'source',
   x.contractId,
-  x.contractNo,
-  x.contractStartDate,
-  x.contractExpirationDate,
-  x.contractComment,
-  x.contractPostpaidMode,
-  x.contractLimit
+  c.title as 'contractNo',
+  coalesce(c.date1, '2042-04-01') as 'contractStartDate',
+  coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
+  c.comment as 'contractComment',
+  c.mode = 0 as 'contractPostpaidMode',
+  c.closesumma as 'contractLimit'
 from
   (
     select
       'Контракт' as 'source',
-      c.id as 'contractId',
-      c.title as 'contractNo',
-      coalesce(c.date1, '2042-04-01') as 'contractStartDate',
-      coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
-      c.comment as 'contractComment',
-      c.mode = 0 as 'contractPostpaidMode',
-      c.closesumma as 'contractLimit'
+      c.id as 'contractId'
     from
       contract c
     where
       convert(c.id, char) = ?
       or c.title regexp ?
       or c.comment regexp ?
+
     union
+
     select
       cpp.title as 'source',
-      c.id as 'contractId',
-      c.title as 'contractNo',
-      coalesce(c.date1, '2042-04-01') as 'contractStartDate',
-      coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
-      c.comment as 'contractComment',
-      c.mode = 0 as 'contractPostpaidMode',
-      c.closesumma as 'contractLimit'
+      cpt1.cid as 'contractId'
     from
-      contract c
-      join contract_parameter_type_1 cpt1 on cpt1.cid = c.id
+      contract_parameter_type_1 cpt1
       left join contract_parameters_pref cpp on cpp.id = cpt1.pid
     where
       cpt1.val regexp ?
+
     union
+
     select
       cpp.title as 'source',
-      c.id as 'contractId',
-      c.title as 'contractNo',
-      coalesce(c.date1, '2042-04-01') as 'contractStartDate',
-      coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
-      c.comment as 'contractComment',
-      c.mode = 0 as 'contractPostpaidMode',
-      c.closesumma as 'contractLimit'
+      cpt3.cid as 'contractId'
     from
-      contract c
-      join contract_parameter_type_3 cpt3 on cpt3.cid = c.id
+      contract_parameter_type_3 cpt3
       left join contract_parameters_pref cpp on cpp.id = cpt3.pid
     where
       cpt3.email regexp ?
+
     union
+
     select
       cpp.title as 'source',
-      c.id as 'contractId',
-      c.title as 'contractNo',
-      coalesce(c.date1, '2042-04-01') as 'contractStartDate',
-      coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
-      c.comment as 'contractComment',
-      c.mode = 0 as 'contractPostpaidMode',
-      c.closesumma as 'contractLimit'
+      cptp.cid as 'contractId'
     from
-      contract c
-      join contract_parameter_type_phone cptp on cptp.cid = c.id
+      contract_parameter_type_phone cptp
       left join contract_parameters_pref cpp on cpp.id = cptp.pid
     where
       cptp.value regexp ?
+
     union
+
     select
       'Заметки' as 'source',
-      c.id as 'contractId',
-      c.title as 'contractNo',
-      coalesce(c.date1, '2042-04-01') as 'contractStartDate',
-      coalesce(c.date2, '2042-04-01') as 'contractExpirationDate',
-      c.comment as 'contractComment',
-      c.mode = 0 as 'contractPostpaidMode',
-      c.closesumma as 'contractLimit'
+      cc.cid as 'contractId'
     from
-      contract c
-      join contract_comment сс on сс.cid = c.id
+      contract_comment cc
     where
-      сс.comment regexp ?
+      cc.comment regexp ?
     group by
-      c.id
+      cc.cid
   ) x
+  join contract c on c.id = x.contractId
 group by
   x.contractId
