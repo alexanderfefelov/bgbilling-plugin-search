401dfe29f185edaa83cb863872af64b5f4b93993
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-21 04:49:38 +0300

    Implement PluginClientListener

---
+++ client/src/main/java/com/github/alexanderfefelov/bgbilling/plugin/search/client/PluginClientListener.java
@@ -0,0 +1,16 @@
+package com.github.alexanderfefelov.bgbilling.plugin.search.client;
+
+import ru.bitel.bgbilling.kernel.plugin.client.inpoint.OnStart;
+import ru.bitel.bgbilling.kernel.plugin.client.inpoint.OnStop;
+
+public class PluginClientListener implements OnStart, OnStop {
+
+   @Override
+   public void onStart() {
+   }
+
+   @Override
+   public void onStop() {
+   }
+
+}
