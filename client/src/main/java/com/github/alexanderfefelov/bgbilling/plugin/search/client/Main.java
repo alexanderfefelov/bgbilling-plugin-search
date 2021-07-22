9b74a3c545dc190673b1d166c7be54700a9108ba
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-07-22 05:53:25 +0300

    Fix birthdate

--- client/src/main/java/com/github/alexanderfefelov/bgbilling/plugin/search/client/Main.java
+++ client/src/main/java/com/github/alexanderfefelov/bgbilling/plugin/search/client/Main.java
@@ -229,7 +229,7 @@ public class Main extends BGUTabPanel {
         }
     }
 
-    private static final String EMPTY_DATE = "2042-04-06";
+    private static final String EMPTY_DATE = "2042-06-04";
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private static final DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
 
